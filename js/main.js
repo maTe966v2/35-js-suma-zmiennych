408550537f09fe4dca08b2dbcf1908eaafecba12
Mateusz Szweda <szweda.mateusz@wp.pl>
2022-03-09 14:56:26 +0100

    initial commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+let firstNumber;
+let secondNumber;
+
+firstNumber = 5;
+secondNumber = 5;
+
+function sum(a, b) {
+    let result = a + b;
+    return result;
+}
+
+sum(firstNumber, secondNumber);
